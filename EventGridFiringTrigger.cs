26a72273ee372fe845ad8b7a59ffdf0dfe325561
Eamonn Byrne <eamonnjames@outlook.com>
2024-11-04 17:28:12 +0000

    message queue for azure service bus

--- EventGridFiringTrigger.cs
+++ EventGridFiringTrigger.cs
@@ -6,23 +6,63 @@ using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using Azure.Messaging.EventGrid;
 using Newtonsoft.Json;
+using Azure.Messaging.ServiceBus;
+using System.Threading.Tasks;
 
-namespace Company.Function
-{
-    public static class EventGridFiringTrigger
-    {
-        [FunctionName("EventGridFiringTrigger")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
-        {
-            var data = JsonConvert.DeserializeObject<EventData>(eventGridEvent.Data.ToString());
-            log.LogInformation($"PlayerId: {data.playerId}, Hit: {data.hit}, HitId: {data.hitId}");
-        }
+namespace Company.Function {
+  public static class EventGridFiringTrigger {
+    static string connectionString = "Endpoint=sb://combat.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SyhyjjSfzzSOvr+1F6fO/erWqts4gKkPo+ASbDB/WZQ=";
+
+    [FunctionName("EventGridFiringTrigger")]
+    public static async Task Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log) {
+      try {
+        var data = JsonConvert.DeserializeObject < EventData > (eventGridEvent.Data.ToString());
+        log.LogInformation($"PlayerId: {data.playerId}, Hit: {data.hit}, HitId: {data.hitId}");
+        await insertSeviceBusMessage(data, log);
+      } catch (Exception ex) {
+        log.LogError($"Error processing EventGridEvent: {ex.Message}");
+        log.LogError(ex.ToString());
+      }
     }
 
-    public class EventData
-    {
-        public string playerId { get; set; }
-        public bool hit { get; set; }
-        public string hitId { get; set; }
+    public static async Task insertSeviceBusMessage(EventData eventData, ILogger log) {
+
+      ServiceBusClient client = new ServiceBusClient(connectionString);
+
+      string queue = eventData.hit ? "hit" : "miss";
+      log.LogInformation($"Queque:{queue}");
+
+      ServiceBusSender sender = client.CreateSender(queue);
+
+      try {
+        string messageBody = JsonConvert.SerializeObject(eventData);
+        ServiceBusMessage message = new ServiceBusMessage(messageBody);
+
+
+       await sender.SendMessageAsync(message);
+
+      } catch (Exception exception) {
+        log.LogInformation($"{DateTime.Now} :: Exception: {exception.Message}");
+      } finally {
+       await sender.DisposeAsync();
+       await client.DisposeAsync();
+      }
     }
+
+  }
+
+  public class EventData {
+    public string playerId {
+      get;
+      set;
+    }
+    public bool hit {
+      get;
+      set;
+    }
+    public string hitId {
+      get;
+      set;
+    }
+  }
 }
